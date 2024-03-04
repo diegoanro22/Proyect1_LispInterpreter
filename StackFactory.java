833c3dae08582767d84d219ea73216a74a49dd84
GenserCatalan <gensercatalanandre@gmail.com>
2024-03-03 22:04:13 -0600

    Se creo la clase StackFactory y se implemento su funcionalidad

--- StackFactory.java
+++ StackFactory.java
@@ -1,4 +1,21 @@
 public class StackFactory<T> {
 
-
+    public StackInterface<T> createStack(String reservedWord) {
+        switch (reservedWord) {
+            case "setQ":
+                return new SetQ<T>();
+            case "defun":
+                return new Defun<T>();
+            case "list":
+                return new List<T>();
+            case "equal":
+                return new Equal<T>();
+            case "quote":
+                return new Quote<T>();
+            case "atom":
+                return new Atom<T>();
+            default:
+                throw new IllegalArgumentException("Palabra reservada no válida: " + reservedWord);
+        }
+    }
 }
