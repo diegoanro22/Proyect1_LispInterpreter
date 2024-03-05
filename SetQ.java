71fb8a60a1ecf379c7ba2836f8e9650c25df2eb0
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2024-03-05 17:22:34 -0600

    SetQ

--- SetQ.java
+++ SetQ.java
@@ -1,3 +1,43 @@
-public class SetQ<T> implements StackInterface<T> {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class SetQ<T> implements StackInterface<T> {
+    private Map<String, T> variableValues;
+    private Scanner scanner;
+
+    public SetQ() {
+        this.variableValues = new HashMap<>();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void execute() {
+        while (true) {
+            String command = scanner.next();
+
+            if (command.equals("setq")) {
+                String variable = scanner.next();
+                T value = (T) scanner.next();
+                setq(variable, value);
+            } else if (command.equals("princ")) {
+                String variable = scanner.next();
+                princ(variable);
+            } else {
+                System.out.println("Comando no reconocido.");
+            }
+        }
+    }
+
+    private void setq(String variable, T value) {
+        variableValues.put(variable, value);
+    }
+
+    private void princ(String variable) {
+        T value = variableValues.get(variable);
+        if (value != null) {
+            System.out.println(value);
+        } else {
+            System.out.println("Variable '" + variable + "' no encontrada.");
+        }
+    }
 }
