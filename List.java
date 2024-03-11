8a037cdbbf4ed49d2eeaa9e73a5b8a700e84041b
GenserCatalan <gensercatalanandre@gmail.com>
2024-03-11 09:49:54 -0600

    Se implemento el funcionamiento de la clase List

--- List.java
+++ List.java
@@ -1,9 +1,30 @@
+import java.util.ArrayList;
+
 public class List<T> implements InterfaceFactory<T> {
 
-    @Override
+    //Metodo para mostrar la lista
     public void execute(String exp) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+        java.util.List<T> list = listConversion(exp);
+        System.out.println(list);
     }
-    
+
+    /**Metodo para convertir la expresion en una lista
+     * @param exp
+     * @return
+     */
+    private java.util.List<T> listConversion(String exp) {
+        String[] tokens = exp.replaceAll("[()]", "").split("\\s+");
+        java.util.List<T> result = new ArrayList<>();
+
+        for (int i = 1; i < tokens.length; i++) {
+            try {
+                T value = (T) tokens[i];
+                result.add(value);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return result;
+    }
 }
