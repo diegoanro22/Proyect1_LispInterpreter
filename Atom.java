90ef105b544551eb02fbadf5056ff5542d9c3ce5
JPLopez23 <jplop2004@gmail.com>
2024-03-11 12:35:41 -0600

    Atom terminado

--- Atom.java
+++ Atom.java
@@ -1,14 +1,24 @@
 public class Atom<T> implements InterfaceFactory<T> {
-    
+
+    private final SetQ<T> setQInstance;
+
+    public Atom(SetQ<T> setQInstance) {
+        this.setQInstance = setQInstance;
+    }
+
     @Override
     public void execute(String exp) {
         String expression = exp.trim();
         if (expression.matches("\\(atom\\s+.+\\)")) {
-            String innerExpression = expression.substring(6, expression.length() - 1).trim();
-            if (isAtom(innerExpression)) {
+            String innerExpression = expression.substring(6, expression.length() - 1).trim(); 
+            if (setQInstance.variableValues.containsKey(innerExpression)) {
                 System.out.println("T");
             } else {
-                System.out.println("NIL");
+                if (isAtom(innerExpression)) {
+                    System.out.println("T");
+                } else {
+                    System.out.println("NIL");
+                }
             }
         }
     }
@@ -17,5 +27,5 @@ public class Atom<T> implements InterfaceFactory<T> {
         boolean isString = expression.matches("\".*\"");
         boolean isInteger = expression.matches("-?\\d+");
         return isString || isInteger;
-    }   
+    }
 }
--- SetQ.java
+++ SetQ.java
@@ -34,3 +34,4 @@ public class SetQ<T> implements InterfaceFactory<T> {
         variableValues.put(variable, value);
     }
 }
+
