24084c41ba9692fea4e8f5b4702752a22c49d3cf
erickguerra2 <eagi578@gmail.com>
2024-03-12 19:26:57 -0600

    Clase equal

--- Equal.java
+++ Equal.java
@@ -2,8 +2,42 @@ public class Equal<T> implements InterfaceFactory<T> {
 
     @Override
     public void execute(String exp) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+        Expression<T> expressionChecker = new Expression<>();
+        try {
+            if (expressionChecker.checkParen(exp)) {
+                exp = exp.substring(0, exp.length() - 1);
+
+                String[] parts = exp.split(" ");
+                if (parts.length < 3) {
+                    System.out.println("Faltan argumentos para 'equal'");
+                    return;
+                }
+
+                String entrada1 = parts[1];
+                String entrada2 =  parts[2];
+
+                System.out.println(equal(entrada1, entrada2));
+            }
+        } catch (Exception e) {
+            System.out.println("Error al procesar la expresión: " + e.getMessage());
+        }
     }
+
+    public String equal(String entrada1, String entrada2) {
+        try {
+            System.out.println(entrada1);
+            System.out.println(entrada2);
+            if (entrada1.equals(entrada2)) {
+                return "T";
+            }
+            else{
+                return "Nil";
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error al procesar los operandos numéricos.");
+        }
+        return "Error";
+    }
+    
     
 }
